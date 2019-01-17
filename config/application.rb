173ad821562bc9c12dc156973a1a1663ec4ad125
UsaMomokawa <spcmomo@gmail.com>
2019-01-17 16:23:49 +0900

    ページング処理を追加

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,11 @@ module BooksApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # デフォルト言語を日本語に設定
+    config.i18n.default_locale = :ja
+    # 辞書ファイルの読み込み
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
+
