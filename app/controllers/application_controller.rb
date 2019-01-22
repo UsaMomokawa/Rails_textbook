7922f75831a718336e7b607542fbc7cc327f065f
UsaMomokawa <42172002+UsaMomokawa@users.noreply.github.com>
2019-01-22 11:22:42 +0900

    英語と日本語に対応 (#3)
    英語と日本語に対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  before_action :set_locale
+
+  # palamsでlocale情報を取得する
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  # default_url_optionsをオーバーライドし、全リンクにlocale情報をセット
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
+  end
 end
