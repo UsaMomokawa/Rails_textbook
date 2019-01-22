27c8da7237e08558e3d7c208f8112a2f49a5bdd0
UsaMomokawa <spcmomo@gmail.com>
2019-01-22 20:06:16 +0900

    ユーザー認証を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   scope "(:locale)" do
+    root 'books#index'
     resources :books
+    devise_for :users
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
 end
