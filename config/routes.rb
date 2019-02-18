7031b0dcaa495bca24a5203096887c47c55c3fda
UsaMomokawa <42172002+UsaMomokawa@users.noreply.github.com>
2019-02-18 23:38:39 +0900

    feature specを書いた

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  # We need to define devise_for just omniauth_callbacks:auth_callbacks otherwise it does not work with scoped locales
-  # see https://github.com/plataformatec/devise/issues/2813
-  devise_for :users, only: :omniauth_callbacks, controllers: {
+  devise_for :users,
+  only: :omniauth_callbacks,
+  controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
-  scope "(:locale)" do
+  scope '(:locale)', locale: /en|ja/ do
     root 'books#index'
+    get '/locale', to: 'books#index'
     resources :books
-    devise_for :users, skip: :omniauth_callbacks, controllers: {
-      registrations: 'users/registrations'
-    }
+    devise_for :users,
+    skip: :omniauth_callbacks
   end
 end
